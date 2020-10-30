5a95c3631f69c92b0ef7fe51475211b2db555bb8
Keshav <56089200+199301200@users.noreply.github.com>
2020-10-30 21:48:40 +0530

    Update deref.cpp

--- deref.cpp
+++ deref.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 struct S {
-	int a;
-	int b;
+	int v;
+	int g;
 	int c;
 };
 
 int main( int argc, char ** argv ) {
 	struct S s = { 1, 2, 3 };
-	printf("s has members a: %d, b: %d, c: %d\n", s.a, s.b, s.c);
+	printf("s has members a: %d, b: %d, c: %d\n", s.v, s.g, s.c);
 	return 0;
 }
